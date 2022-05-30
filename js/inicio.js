6a2d7642776c7419bfa07536b763d84507a5c314
Litriboxer <litriboxer@hotmail.com>
2022-05-30 16:29:15 +0200

    GUARDADO DE CAMBIOS

--- js/inicio.js
+++ js/inicio.js
@@ -4,14 +4,14 @@ const closeEls = document.querySelectorAll("[data-close]");
 const isVisible = "is-visible";
 
 for (const el of openEls) {
-  el.addEventListener("click", function() {
+  el.addEventListener("click", function () {
     const modalId = this.dataset.open;
-    document.getElementById(modalId).classList.add(isVisible); 
+    document.getElementById(modalId).classList.add(isVisible);
   });
 }
 
 for (const el of closeEls) {
-  el.addEventListener("click", function() {
+  el.addEventListener("click", function () {
     this.parentElement.parentElement.parentElement.classList.remove(isVisible);
   });
 }
@@ -31,6 +31,6 @@ document.addEventListener("keyup", e => {
 
 
 function playOn() {
-var audio = new Audio("/multimedia/sonidos/clickSound.mp3");
-audio.play();
+  var audio = new Audio("/multimedia/sonidos/clickSound.mp3");
+  audio.play();
 }
